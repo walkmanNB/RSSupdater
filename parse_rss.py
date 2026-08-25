2d6b0d709dabcc7dc2e528e428c53d4aa036d01f
walkmanNB <walkmaninhongkong@gmail.com>
2026-08-25 10:45:37 +0800

    Update parse_rss.py

--- parse_rss.py
+++ parse_rss.py
@@ -6,7 +6,8 @@ from datetime import datetime
 SOURCES = [
     {"name": "大纪元新闻", "url": "https://feed.epochtimes.com/gb/feed", "type": "news"},
     {"name": "TED Radio Hour", "url": "https://feeds.npr.org/510298/podcast.xml", "type": "podcast"},
-    {"name": "美国之音中文网", "url": "https://www.voachinese.com/api/zm_yql-vomx-tpeybti", "type": "news"}
+    {"name": "美国之音中文网", "url": "https://www.voachinese.com/api/zm_yql-vomx-tpeybti", "type": "news"},
+    {"name": "美国之音中文广播", "url": "https://www.voachinese.com/api/zviqoyl-vomx-tpeugiov", "type": "podcast"}
 ]
 
 all_items = []
