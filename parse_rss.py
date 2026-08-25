3d6fa342e441a056bb631d358f1316f66510400f
walkmanNB <walkmaninhongkong@gmail.com>
2026-08-25 10:41:27 +0800

    Update parse_rss.py

--- parse_rss.py
+++ parse_rss.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 # 你可以把你的 RSS 源配置在这里，或者读取一个 opml/json 文件
 SOURCES = [
-    {"name": "大纪元新闻", "url": "https://feed.epochtimes.com/feed", "type": "news"},
+    {"name": "大纪元新闻", "url": "https://feed.epochtimes.com/gb/feed", "type": "news"},
     {"name": "TED Radio Hour", "url": "https://feeds.npr.org/510298/podcast.xml", "type": "podcast"},
     {"name": "美国之音中文网", "url": "https://www.voachinese.com/api/zm_yql-vomx-tpeybti", "type": "news"}
 ]
